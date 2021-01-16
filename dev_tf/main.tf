8b37da12a8da3347ec51bd3cf2ef1c3ccd428440
ajinkya-bhabal-ifi <ajinkya.bhabal@ifi.tech>
2021-01-16 20:44:31 +0530

    16_jan_2021 1st release

--- dev_tf/main.tf
+++ dev_tf/main.tf
@@ -1,7 +1,7 @@
 # Create a Resource Group
 resource "azurerm_resource_group" "rg1" {
   name     = "${var.env_name}-Terraform-RG1"
-  location = var.location 
+  location = var.location
 }
 
 # Create a Virtual Network
@@ -17,6 +17,15 @@ resource "azurerm_virtual_network" "vnet1" {
 resource "azurerm_subnet" "subnet1" {
   name                 = "First_subnet"                     # name of subnet
   address_prefixes     = ["10.10.1.0/24"]                   # address space of subnet
+  virtual_network_name = azurerm_virtual_network.vnet1.name # name of virtual network in which subnet will part of it
+  resource_group_name  = azurerm_resource_group.rg1.name    # resource group in which resource will be deployed
+  depends_on           = [azurerm_virtual_network.vnet1]    # setting up an explicit dependency on vnet
+}
+
+# Create a Subnet in Virtual Network
+resource "azurerm_subnet" "subnet2" {
+  name                 = "Second_subnet"                    # name of subnet
+  address_prefixes     = ["10.10.2.0/24"]                   # address space of subnet
   virtual_network_name = azurerm_virtual_network.vnet1.name # name of virtual network in which subnet will part of it
   resource_group_name  = azurerm_resource_group.rg1.name    # resource group in which resource will be deployed
   depends_on           = [azurerm_virtual_network.vnet1]    # setting up an explicit dependency on vnet
